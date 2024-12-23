e73c7e7f47a7d17d5d3dd81d9095c003b5a49c42
brakchen <907919772@qq.com>
2024-12-23 23:18:03 +0800

    fix

---
+++ ylb-backend/src/main/java/com/ylb/ylbbackend/YlbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ylb.ylbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YlbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YlbBackendApplication.class, args);
+	}
+
+}
---
+++ ylb-backend/src/test/java/com/ylb/ylbbackend/YlbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ylb.ylbbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YlbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
